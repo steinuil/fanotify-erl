6e5a007e5eec5c646c05f264dd9340406073a317
steinuil <steenuil.owl@gmail.com>
2025-07-27 15:52:29 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            erlang_26
+            erlang-ls
+            rebar3
+
+            (clang-tools.override {
+              enableLibcxx = false;
+            })
+            gnumake
+            bear
+          ];
+
+          shellHook = ''
+            mkdir -p .erlang
+            export MIX_HOME=$PWD/.erlang/mix
+            export HEX_HOME=$PWD/.erlang/hex
+            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
+
+            export PATH=$MIX_HOME/bin:$PATH
+            export PATH=$MIX_HOME/escripts:$PATH
+            export PATH=$HEX_HOME/bin:$PATH
+
+            export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang/history\"'"
+          '';
+        };
+
+      }
+    );
+}
